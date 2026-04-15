94c7c30172298528d5129a6c129171dd7e6ee52e
chienpv109 <chienboybu1092004@gmail.com>
2026-04-15 17:40:26 +0700

    add link frontend to deploy

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
-  "http://127.0.0.1:5173"
+  "http://127.0.0.1:5173",
+  "https://estate-market01-smoky.vercel.app"
 ];
 
 if (process.env.CLIENT_URL) {
